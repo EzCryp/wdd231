520d39d818aea6cc0d1d309b69367b139c3f047d
Ezcryp <u637424283@byui.edu>
2025-08-13 12:23:10 +0800

    discover page

---
+++ chamber/data/places.mjs
@@ -0,0 +1,73 @@
+// 10 places with name, address, description, cost, image url 
+export const places = [
+    {
+        "name": "Night Markets",
+        "address": "Shilin, Taipei",
+        "description": "Wander through places like Shilin Night Market (Taipei) or Liuhe Night Market (Kaohsiung) for street food, games, and quirky souvenirs.",
+        "cost": "100Php",
+        "imageUrl": "images/night-market.webp"
+    },
+    {
+        "name": "Sun Moon Lake Cycling",
+        "address": "Taiwan Central, Taipei",
+        "description": "Explore the scenic Sun Moon Lake by bike. Enjoy the beautiful views and fresh air while cycling around the lake.",
+        "cost": "200Php",
+        "imageUrl": "images/sun-moon.webp"
+    },
+    {
+        "name": "Beitou Hot Springs",
+        "address": "Beitou, Taipei",
+        "description": "Relax and unwind in the natural hot springs of Taiwan. Enjoy the therapeutic benefits and beautiful surroundings.",
+        "cost": "300Php",
+        "imageUrl": "images/hot-springs.webp"
+    },
+    {
+        "name": "Taipei 101 Observation Deck",
+        "address": "Taipei city, Taipei",
+        "description": "Enjoy panoramic views of Taipei from the observation deck of Taipei 101, one of the tallest buildings in the world.",
+        "cost": "400Php",
+        "imageUrl": "images/taipe-101.webp"
+    },
+    {
+        "name": "Pingxi Sky Lantern Festival",
+        "address": "Pingxi, Taipei",
+        "description": "Release a sky lantern into the night sky at the Pingxi Sky Lantern Festival. Write your wishes on the lantern and watch it float away.",
+        "cost": "50Php",
+        "imageUrl": "images/sky-lantern.webp"
+    },
+    {
+        "name": "Jiufen Old Street",
+        "address": "Jiufen, Taipei",
+        "description": "Stroll through the charming streets of Jiufen, known for its tea houses, street food, and stunning views of the mountains and sea.",
+        "cost": "60Php",
+        "imageUrl": "images/jiufen-oldstreet.webp"
+    },
+    {
+        "name": "Taroko Gorge Hike",
+        "address": "Taroko, Taipei",
+        "description": "Experience the breathtaking beauty of Taroko Gorge. Hike through stunning marble cliffs, lush forests, and crystal-clear rivers.",
+        "cost": "70Php",
+        "imageUrl": "images/taroko-hike.webp"
+    },
+    {
+        "name": "Tainan’s Temples & Street Food",
+        "address": "Tainan, Taipei",
+        "description": "Explore the rich history and culture of Tainan by visiting its ancient temples and indulging in delicious street food.",
+        "cost": "80Php",
+        "imageUrl": "images/street-food.webp"
+    },
+    {
+        "name": "Surfing or Beach Time in Kenting",
+        "address": "Kenting, Taipei",
+        "description": "Enjoy the sun, sand, and surf at Kenting National Park. Relax on the beach or try your hand at surfing in the beautiful waters.",
+        "cost": "900Php",
+        "imageUrl": "images/kenting-beach.webp"
+    },
+    {
+        "name": "Alishan Sunrise",
+        "address": "Alishan, Taipei",
+        "description": "Witness the breathtaking sunrise at Alishan. Take a scenic train ride and enjoy the stunning views as the sun rises over the mountains.",
+        "cost": "10Php",
+        "imageUrl": "images/alishan-sunrise.webp"
+    }
+]
