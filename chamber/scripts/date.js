4fd35c52b8714e80a5259b386c8e97189afb9d55
Ezcryp <u637424283@byui.edu>
2025-09-27 00:22:49 +0800

    Assignment W02, W03, W04

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,6 +1,6 @@
-// last modified and current year display
+ // last modified and current year display
 document.getElementById("lastModified").textContent = document.lastModified;
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-const lastModifiedElement = document.querySelector("#lastModified");
+constlastModifiedElement = document.querySelector("#lastModified");
 lastModifiedElement.style.color = "#ffd05c";
